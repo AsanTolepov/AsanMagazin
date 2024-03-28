91e94c54e5c578862746a20381f975147a18f9c4
AsanTolepov <asantolepov03@gmail.com>
2024-03-28 10:28:34 +0500

    add

--- src/slice/Product.Slice.js
+++ src/slice/Product.Slice.js
@@ -51,7 +51,7 @@ export const productSlice = createSlice({
                 item.id === action.payload ? { ...item, quantyti: item.quantyti + 1 } : item
             );
             const data = state.basket.find(element => +element.id === +action.payload)
-            state.totalPrice += data.price
+            state.totalPrice += Math.round(data.price)
         },
         decrementQuantity(state, action) {
             state.basket = state.basket.map(item =>
@@ -61,7 +61,7 @@ export const productSlice = createSlice({
             if(state.totalPrice===0){
                 return;
             }else{
-                state.totalPrice -= data.price
+                state.totalPrice -= Math.round(data.price)
             }
         },
         setSelectProducts(state, action){
